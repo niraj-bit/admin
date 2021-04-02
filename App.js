174cd9545c43b51171d6e172822f2b08c30ed725
niraj-bit <81625158+niraj-bit@users.noreply.github.com>
2021-04-03 00:49:28 +0530

    init

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import{Admin,Resource}from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import PostList from './components/postList'
+ 
+
+function App() {
+  return (
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+    <Resource name ='posts' list={PostList} />
+
+    </Admin>
+  )
+
+}
+
+export default App;
